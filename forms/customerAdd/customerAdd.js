361d260ebcad0b99748796c70c31613595eeb60a
Ethan Hall <ethanhall99@gmail.com>
2021-03-22 15:45:04 -0500

    Update customerAdd.js

--- forms/customerAdd/customerAdd.js
+++ forms/customerAdd/customerAdd.js
@@ -9,7 +9,7 @@ let zip = ""
 customerAdd.onshow=function(){
 
   query = "SELECT name FROM customer"
-  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=ekh74960&pass=" + pw + "&database=ekh74960&query=" + query)
+  req = Ajax("https://ormond.creighton.edu/courses/375/ajax-connection.php", "POST", "host=ormond.creighton.edu&user=" + netID + "&pass=" + pw + "&database=" + netID + "&query=" + query)
 
   if (req.status == 200) { 
     results = JSON.parse(req.responseText)
